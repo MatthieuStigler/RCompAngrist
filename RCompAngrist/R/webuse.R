5d2d3dbde71b598d04bb0b1adc04281bde562640
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2014-04-15 13:20:16 +0530

    webuse: remove the require()

--- RCompAngrist/R/webuse.R
+++ RCompAngrist/R/webuse.R
@@ -4,12 +4,11 @@
 #' @param x The name of the dataset, as character
 #' @return The output from read.dta
 #' @export
-#' @import foreign
+#' @importFrom foreign read.dta
 
 
 webuse <- function(x){
   if(!is.character(x)) stop("'x' should be character")
-  require(foreign)
   link <- paste("http://www.stata-press.com/data/r12/", x, ".dta", sep="")  
   
   out <- read.dta(link)
